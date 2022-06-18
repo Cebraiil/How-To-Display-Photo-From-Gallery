1fd5fb98ff6514ec1ed709fe4d111c69b706f142
Dev Light <106925514+developerlight@users.noreply.github.com>
2022-06-18 14:11:00 +0300

    Initial Commit

---
+++ ImagePicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImagePicker
+//
+//  Created by Developer Light on 18.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImagePicker/ImagePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePickerApp.swift
+//  ImagePicker
+//
+//  Created by Developer Light on 18.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ImagePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
