462a5ee89d68244055885bce9d0bdabe53a04407
Dev Light <106925514+developerlight@users.noreply.github.com>
2022-06-18 17:24:38 +0300

    Added Codes!

--- ImagePicker/ContentView.swift
+++ ImagePicker/ContentView.swift
@@ -2,15 +2,42 @@
 //  ContentView.swift
 //  ImagePicker
 //
-//  Created by Developer Light on 18.06.2022.
+// by Developer Light on 18.06.2022.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isShowPhotoLibrary = false
+    @State private var image = UIImage()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image(uiImage: self.image)
+                .resizable()
+                .scaledToFit()
+                .frame(minWidth:0, maxWidth: .infinity)
+                .edgesIgnoringSafeArea(.all)
+            
+            Button {
+                self.isShowPhotoLibrary = true
+            } label: {
+                HStack {
+                    Image(systemName: "photo")
+                        .font(.system(size: 20))
+                    
+                    Text("Photo Library")
+                }
+                .frame(minWidth:0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
+                .background(Color.primary)
+                .foregroundColor(.white)
+                .cornerRadius(1)
+                .padding(.horizontal)
+            }
+        }
+        .sheet(isPresented: $isShowPhotoLibrary) {
+            ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
+        }
     }
 }
 
